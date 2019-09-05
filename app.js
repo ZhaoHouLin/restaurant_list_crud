ce22605f041e5884353234160176769c4d03411e
Raiden <rodes5292@gmail.com>
2019-09-05 14:13:44 +0800

    Add edit page and update action

--- app.js
+++ app.js
@@ -71,11 +71,27 @@ app.post("/restaurants", (req, res) => {
 });
 // 修改 餐廳 頁面
 app.get("/restaurants/:id/edit", (req, res) => {
-  res.send("修改 Todo 頁面");
+  Restaurant.findById(req.params.id, (err, restaurant) => {
+    if (err) return console.error(err);
+    return res.render("edit", { restaurant: restaurant });
+  });
 });
-// 修改 餐廳
+// 修改 餐廳 資料
 app.post("/restaurants/:id/edit", (req, res) => {
-  res.send("修改 Todo");
+  Restaurant.findById(req.params.id, (err, restaurant) => {
+    if (err) return console.error(err);
+    restaurant.name = req.body.name;
+    restaurant.category = req.body.category;
+    restaurant.image = req.body.image;
+    restaurant.location = req.body.location;
+    restaurant.phone = req.body.phone;
+    restaurant.google_map = req.body.google_map;
+    restaurant.description = req.body.description;
+    restaurant.save(err => {
+      if (err) return console.error(err);
+      return res.redirect(`/restaurants/${req.params.id}`);
+    });
+  });
 });
 // 刪除 餐廳
 app.post("/restaurants/:id/delete", (req, res) => {
